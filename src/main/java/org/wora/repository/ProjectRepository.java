64231e2106665e2859a77be6fd97eaa218255b3a
Maleknoura <maleknoura098@gmail.com>
2024-09-17 19:18:25 +0100

    creation des methodes dans projectRepository

--- src/main/java/org/wora/repository/ProjectRepository.java
+++ src/main/java/org/wora/repository/ProjectRepository.java
@@ -1,4 +1,20 @@
 package org.wora.repository;
 
+import org.wora.entity.Project;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProjectRepository {
+
+    void createProject(Project project);
+
+    Optional<Project> getProjectById(int projectId);
+
+    List<Project> getAllProjects();
+
+    void updateProject(Project project);
+
+    void deleteProject(int projectId);
+
 }
